22e1b017d7f1e8e968cdf1b453972c06aad1a2c4
amirhamrouni <amiroscrypto2022@gmail.com>
2026-09-24 01:20:59 +0200

    fix(auth): restore original Capacitor Firebase auth mode

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -7,7 +7,7 @@ const config: CapacitorConfig = {
   plugins: {
     FirebaseAuthentication: {
       providers: ['google.com'],
-      skipNativeAuth: true,
+      skipNativeAuth: false,
     },
   },
 };
