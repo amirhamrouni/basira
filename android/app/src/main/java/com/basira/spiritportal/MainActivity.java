403163100b35832e5dc6be1f1d26e30c9aaffb4c
amirhamrouni <amiroscrypto2022@gmail.com>
2026-09-23 23:19:09 +0200

    fix(android): explicitly register Firebase Authentication plugin

--- android/app/src/main/java/com/basira/spiritportal/MainActivity.java
+++ android/app/src/main/java/com/basira/spiritportal/MainActivity.java
@@ -1,5 +1,14 @@
 package com.basira.spiritportal;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import io.capawesome.capacitorjs.plugins.firebase.authentication.FirebaseAuthenticationPlugin;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        // Register explicitly so authentication cannot silently disappear from a packaged APK.
+        registerPlugin(FirebaseAuthenticationPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
