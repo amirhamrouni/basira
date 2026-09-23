7617f324abc8fa7f39aea8b8ed96f1586f04887a
amirhamrouni <amiroscrypto2022@gmail.com>
2026-09-23 22:25:40 +0200

    test: align structured reading parser expectations

--- src/components/BasiraReadingText.test.tsx
+++ src/components/BasiraReadingText.test.tsx
@@ -5,8 +5,14 @@ describe('parseBasiraReading', () => {
   it('splits bracketed BASIRA V2 headings into sections', () => {
     const sections = parseBasiraReading('[أقوى 3 إشارات]\nعلامة أولى\n\n[التوقيت]\nخلال أسابيع');
     expect(sections).toEqual([
-      { title: 'أقوى 3 إشارات', body: ['علامة أولى', ''] },
+      { title: 'أقوى 3 إشارات', body: ['علامة أولى'] },
       { title: 'التوقيت', body: ['خلال أسابيع'] }
+    ]);
+  });
+
+  it('accepts markdown-wrapped headings', () => {
+    expect(parseBasiraReading('### **[تنبيه بصيرة]**\nانتبه للتسرع')).toEqual([
+      { title: 'تنبيه بصيرة', body: ['انتبه للتسرع'] }
     ]);
   });
 
